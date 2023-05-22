c7d3b34118820d7da2d2fa6507d06b64e3485cb7
DogukanDursun <dod63y@gmail.com>
2023-05-23 00:24:57 +0300

    SignalRChat

---
+++ SignalRChatDemo/Hubs/ChatHubs.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRChatDemo.Hubs
+{
+    public class ChatHubs:Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            // Gelen mesajı tüm istemcilere iletilmesi için istemci metodunu çağırın
+            await Clients.All.SendAsync("ReceiveMessage",user, message);
+        }
+    }
+}
